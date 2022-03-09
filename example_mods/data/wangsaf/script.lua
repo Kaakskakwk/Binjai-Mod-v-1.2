91001bc6c35d0f74f27b2bfda2375ebcee84cdab
k2kisbadatcoding <71873309+Kaakskakwk@users.noreply.github.com>
2022-03-09 21:43:46 +0700

    omgguys

---
+++ example_mods/data/wangsaf/script.lua
@@ -0,0 +1,6 @@
+function onCreate()
+    makeLuaSprite('wangsaf', 'iyhdek', -580, -90);
+    scaleLuaSprite('wangsaf', 10, 10);
+    addLuaSprite('wangsaf', true)
+    setPropertyLuaSprite('wangsaf', 'alpha', 0)
+end
